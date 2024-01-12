fe890118d8712534f2283b4426a7c3d12811c354
7anane-b <144293971+7anane-b@users.noreply.github.com>
2024-01-12 14:42:25 -0500

    inverse

--- scripts.js
+++ scripts.js
@@ -19,12 +19,25 @@
             return y
         
         }
-        
+
+                
         function clearScreen(){
 
             document.getElementById('resulta').value = ''
         
         }
 
+        function inverse(){
+           let val = document.getElementById('resulta').value;
+           let newVal = eval(val);
+           if(newVal != 0){
+            newVal = -newVal;
+           document.getElementById('resulta').value = newVal;
+           return newVal;
+        }
+
+        }
+
 
 document.getElementById('plus').addEventListener('click', affiche('+'));
+
